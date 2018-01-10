ac6d5359b61521e882470719c121549f57cb09a9
stefanik12 <stefanik.mich@gmail.com>
2018-01-10 12:32:54 +0100

    added main script into answers_classifier.py

---
+++ answers_classifier.py
@@ -0,0 +1,19 @@
+# interchangeable model loader
+# a model is supposed to be trained externally
+from sklearn.externals import joblib
+
+from answers_downloader import download_answer_sheet
+from answers_transform import transform_answers
+from fill_data import write_results
+
+
+sheet_filled = download_answer_sheet()
+sheet_classifiable = transform_answers(sheet_filled)
+
+classifier_model = joblib.load("logistic_regression_trained.mod")
+probs = classifier_model.predict_proba(X=sheet_classifiable.value)
+
+print probs
+
+# write_results(probs)
+
